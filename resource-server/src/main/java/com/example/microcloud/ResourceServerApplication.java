957afa1b7ec5caa9a7ba16a6a9e11bf24ecc2834
hadi <t@t.com>
2022-02-15 18:31:30 +0330

    init project

---
+++ resource-server/src/main/java/com/example/microcloud/ResourceServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
